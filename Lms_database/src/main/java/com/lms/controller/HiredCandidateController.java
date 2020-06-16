001eb4bc95c21ca893b86165a7a75b2777f8ae1c
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-16 23:02:32 +0530

    MODIFIED:Changes in code added hired candidate list api

--- Lms_database/src/main/java/com/lms/controller/HiredCandidateController.java
+++ Lms_database/src/main/java/com/lms/controller/HiredCandidateController.java
@@ -1,8 +1,11 @@
 package com.lms.controller;
+import java.util.List;
+
 import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +27,12 @@ public class HiredCandidateController {
 		 boolean response=hiredCandidateService.getHiredCandidate(filePath);
 	        return new ResponseEntity<Response>(new Response(200,ApplicationConfiguration.getMessageAccessor()
 	                .getMessage("109"),response), HttpStatus.CREATED);
+	    }
+	 
+	 @GetMapping("/hiredcandidatelist")
+	    public ResponseEntity<List> hiredCandidate() {
+	        List list = hiredCandidateService.getAllHiredCandidates();
+	        return new ResponseEntity<List>(list, HttpStatus.MULTI_STATUS);
 	    }
 
 }
