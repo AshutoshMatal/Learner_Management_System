54895e81fd0ef737fbbe1d92f76fa59003fbd6c0
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-16 18:16:09 +0530

    Added:Application configuration

---
+++ Lms_database/src/main/java/com/lms/configuration/ApplicationConfiguration.java
@@ -0,0 +1,28 @@
+package com.lms.configuration;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+@EnableAutoConfiguration
+public class ApplicationConfiguration {
+
+    private static MessageSourceAccessor messageSourceAccessor;
+
+    @PostConstruct
+    private static void initMessageSourceAccessor() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:message");
+        messageSourceAccessor = new MessageSourceAccessor(messageSource, Locale.getDefault());
+    }
+
+    public static MessageSourceAccessor getMessageAccessor() {
+        return messageSourceAccessor;
+    }
+}
