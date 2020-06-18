c3150b9d9f46c3efc06f8920beed0110e14ba583
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-18 16:56:55 +0530

    MODIFIED:Proper indentation and Remove unwanted space and changes in code to resolve the error

--- Lms_database/src/main/java/com/lms/repository/HiredCandidateRepository.java
+++ Lms_database/src/main/java/com/lms/repository/HiredCandidateRepository.java
@@ -1,5 +1,11 @@
 package com.lms.repository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lms.model.HiredCandidate;
 
-public interface HiredCandidateRepository {
+public interface HiredCandidateRepository extends JpaRepository<HiredCandidate ,Integer>{
+	Optional<HiredCandidate> findByEmail( String email);
 
+	List<HiredCandidate> findAll();	  
 }
