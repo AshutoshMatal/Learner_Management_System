95af90f6b4f83296d2bebab5442f34d5bb9cb45b
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-18 16:38:37 +0530

    MODIFIED:Proper indentation and changes in code to resolve error

--- Lms_database/src/main/java/com/lms/dto/UserRegisterDTO.java
+++ Lms_database/src/main/java/com/lms/dto/UserRegisterDTO.java
@@ -1,48 +1,20 @@
 package com.lms.dto;
-
-
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.NotEmpty;
-//import javax.validation.constraints.Pattern;
-
 public class UserRegisterDTO 
 {
 	//VARIABLES
-//	@Email(message = "Please provide a valid e-mail")
-//	@NotEmpty(message = "Please provide an e-mail")
 	private String email;
-	//@NotEmpty(message = "Please provide your user name")
+
 	private String first_name;
-	//@NotEmpty(message = "Please provide your name")
+
 	private String last_name;
-	//@NotEmpty(message = "Please provide your name")
+
 	private String password;
-	//@Pattern(regexp = "([0-9]{10})")
+
 	private long contact_number;
-	//@NotEmpty(message = "Please provide your name")
+
 	private String creator_user;
+
 	//CONSTRUCTOR
-//	public UserRegisterDTO(
-//			//@Email(message = "Please provide a valid e-mail") @NotEmpty(message = "Please provide an e-mail")
-//			String email,
-//			//@NotEmpty(message = "Please provide your user name") 
-//			String first_name,
-//			//@NotEmpty(message = "Please provide your name")
-//			String last_name,
-//			//@NotEmpty(message = "Please provide your name") 
-//			String password,
-//			//@Pattern(regexp = "([0-9]{10})")
-//			long contact_number,
-//			//@NotEmpty(message = "Please provide your name")
-//			String creator_user) {
-//		super();
-//		this.email = email;
-//		this.first_name = first_name;
-//		this.last_name = last_name;
-//		this.password = password;
-//		this.contact_number = contact_number;
-//		this.creator_user = creator_user;
-//	}
 	public String getEmail() {
 		return email;
 	}
@@ -79,7 +51,5 @@ public class UserRegisterDTO
 	public void setCreator_user(String creator_user) {
 		this.creator_user = creator_user;
 	}
-
-
 }
 
