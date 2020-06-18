00b25c44934fce260896d152e8cdb65e362b2d69
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-18 16:47:40 +0530

    MODIFIED:Proper indentation and Remove unwanted space

--- Lms_database/src/main/java/com/lms/message/JwtToken.java
+++ Lms_database/src/main/java/com/lms/message/JwtToken.java
@@ -1,7 +1,9 @@
 package com.lms.message;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.lms.exception.InvalidTokenException;
@@ -11,33 +13,31 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
-	public class JwtToken {
-	
+public class JwtToken {
+
 	@Autowired
 	MessageInfo message;
-	
-		SignatureAlgorithm algorithm = SignatureAlgorithm.HS256;
-		
-		// SECREAT KEY
-		static String secretKey = "iamsecretkey";
 
-		// GENERATE TOKEN
-		public String generateToken(String emailId) {
-			return Jwts.builder().setId(emailId).setIssuedAt(new Date(System.currentTimeMillis()))
-					.signWith(algorithm, secretKey).compact();
-		}
+	SignatureAlgorithm algorithm = SignatureAlgorithm.HS256;
 
-		// DECODE TOKEN
-		public String getToken(String token) {
-			Claims claim = null;
-			try {
-				claim = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-			} catch (Exception e) {
-				throw new InvalidTokenException(400,message.Invalide_Token);
-			}
-			return claim.getId();
-		}
+	// SECREAT KEY
+	static String secretKey = "iamsecretkey";
+
+	// GENERATE TOKEN
+	public String generateToken(String emailId) {
+		return Jwts.builder().setId(emailId).setIssuedAt(new Date(System.currentTimeMillis()))
+				.signWith(algorithm, secretKey).compact();
 	}
-
+	// DECODE TOKEN
+	public String getToken(String token) {
+		Claims claim = null;
+		try {
+			claim = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			throw new InvalidTokenException(400,message.Invalide_Token);
+		}
+		return claim.getId();
+	}
+}
 
 
