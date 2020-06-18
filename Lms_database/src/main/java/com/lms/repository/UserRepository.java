5d9c163cac826ad7453f6638cb93d41145588c3a
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-18 16:55:58 +0530

    MODIFIED:Proper indentation and Remove unwanted space

--- Lms_database/src/main/java/com/lms/repository/UserRepository.java
+++ Lms_database/src/main/java/com/lms/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.lms.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.lms.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findByEmail(String email);
-
 }
