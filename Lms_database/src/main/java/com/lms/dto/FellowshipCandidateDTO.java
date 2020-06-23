e2ae3ccac242a67da215ea7e4942645ba4345f66
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-23 17:11:55 +0530

    ADDED:Fellowship candidate dto  is added

---
+++ Lms_database/src/main/java/com/lms/dto/FellowshipCandidateDTO.java
@@ -0,0 +1,241 @@
+package com.lms.dto;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class FellowshipCandidateDTO {
+	private int id;
+	private String first_name;
+	private String middle_name;
+	private String last_name;
+	private String email;
+	private String hired_city;
+	private String degree;
+	private Date hired_date;
+	private int contact_number;
+	private int permanent_pincode;
+	private String hired_lab;
+	private String attitude;
+	private String communication_remark;
+	private String knowledge_remark;
+	private String aggregate_remark;
+	private String status;
+	private LocalDateTime creator_stamp;
+	private String creator_user;
+	private String birth_date;
+	private String is_birth_date_verified;
+	private String parent_occupation;
+	private String parent_contact_number;
+	private String parent_annual_salary;
+	private String local_address;
+	private String permanent_address;
+	private String photo_path;
+	private String joining_date;
+	private String candidate_status;
+	private String personal_information;
+	private String bank_information;
+	private String educational_information;
+	private String document_status;
+	private String remark;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getMiddle_name() {
+		return middle_name;
+	}
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHired_city() {
+		return hired_city;
+	}
+	public void setHired_city(String hired_city) {
+		this.hired_city = hired_city;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public Date getHired_date() {
+		return hired_date;
+	}
+	public void setHired_date(Date hired_date) {
+		this.hired_date = hired_date;
+	}
+	public int getContact_number() {
+		return contact_number;
+	}
+	public void setContact_number(int contact_number) {
+		this.contact_number = contact_number;
+	}
+	public int getPermanent_pincode() {
+		return permanent_pincode;
+	}
+	public void setPermanent_pincode(int permanent_pincode) {
+		this.permanent_pincode = permanent_pincode;
+	}
+	public String getHired_lab() {
+		return hired_lab;
+	}
+	public void setHired_lab(String hired_lab) {
+		this.hired_lab = hired_lab;
+	}
+	public String getAttitude() {
+		return attitude;
+	}
+	public void setAttitude(String attitude) {
+		this.attitude = attitude;
+	}
+	public String getCommunication_remark() {
+		return communication_remark;
+	}
+	public void setCommunication_remark(String communication_remark) {
+		this.communication_remark = communication_remark;
+	}
+	public String getKnowledge_remark() {
+		return knowledge_remark;
+	}
+	public void setKnowledge_remark(String knowledge_remark) {
+		this.knowledge_remark = knowledge_remark;
+	}
+	public String getAggregate_remark() {
+		return aggregate_remark;
+	}
+	public void setAggregate_remark(String aggregate_remark) {
+		this.aggregate_remark = aggregate_remark;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public LocalDateTime getCreator_stamp() {
+		return creator_stamp;
+	}
+	public void setCreator_stamp(LocalDateTime creator_stamp) {
+		this.creator_stamp = creator_stamp;
+	}
+	public String getCreator_user() {
+		return creator_user;
+	}
+	public void setCreator_user(String creator_user) {
+		this.creator_user = creator_user;
+	}
+	public String getBirth_date() {
+		return birth_date;
+	}
+	public void setBirth_date(String birth_date) {
+		this.birth_date = birth_date;
+	}
+	public String getIs_birth_date_verified() {
+		return is_birth_date_verified;
+	}
+	public void setIs_birth_date_verified(String is_birth_date_verified) {
+		this.is_birth_date_verified = is_birth_date_verified;
+	}
+	public String getParent_occupation() {
+		return parent_occupation;
+	}
+	public void setParent_occupation(String parent_occupation) {
+		this.parent_occupation = parent_occupation;
+	}
+	public String getParent_contact_number() {
+		return parent_contact_number;
+	}
+	public void setParent_contact_number(String parent_contact_number) {
+		this.parent_contact_number = parent_contact_number;
+	}
+	public String getParent_annual_salary() {
+		return parent_annual_salary;
+	}
+	public void setParent_annual_salary(String parent_annual_salary) {
+		this.parent_annual_salary = parent_annual_salary;
+	}
+	public String getLocal_address() {
+		return local_address;
+	}
+	public void setLocal_address(String local_address) {
+		this.local_address = local_address;
+	}
+	public String getPermanent_address() {
+		return permanent_address;
+	}
+	public void setPermanent_address(String permanent_address) {
+		this.permanent_address = permanent_address;
+	}
+	public String getPhoto_path() {
+		return photo_path;
+	}
+	public void setPhoto_path(String photo_path) {
+		this.photo_path = photo_path;
+	}
+	public String getJoining_date() {
+		return joining_date;
+	}
+	public void setJoining_date(String joining_date) {
+		this.joining_date = joining_date;
+	}
+	public String getCandidate_status() {
+		return candidate_status;
+	}
+	public void setCandidate_status(String candidate_status) {
+		this.candidate_status = candidate_status;
+	}
+	public String getPersonal_information() {
+		return personal_information;
+	}
+	public void setPersonal_information(String personal_information) {
+		this.personal_information = personal_information;
+	}
+	public String getBank_information() {
+		return bank_information;
+	}
+	public void setBank_information(String bank_information) {
+		this.bank_information = bank_information;
+	}
+	public String getEducational_information() {
+		return educational_information;
+	}
+	public void setEducational_information(String educational_information) {
+		this.educational_information = educational_information;
+	}
+	public String getDocument_status() {
+		return document_status;
+	}
+	public void setDocument_status(String document_status) {
+		this.document_status = document_status;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+
+}
