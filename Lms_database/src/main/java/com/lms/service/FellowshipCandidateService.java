d9e6b6d66823bb8a2a79742e001cafa23639f0a0
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-06-23 17:18:28 +0530

    ADDED:Fellowship candidate interface  having a method onBoard accepted candidate

---
+++ Lms_database/src/main/java/com/lms/service/FellowshipCandidateService.java
@@ -0,0 +1,7 @@
+package com.lms.service;
+import com.lms.model.FellowshipCandidate;
+
+public interface FellowshipCandidateService {
+	 public FellowshipCandidate onboardAcceptedCandidates(int id);
+	 
+}
