0403bd425042d135597f880742c14bf5101b5382
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-07-13 18:44:56 +0530

    MODIFIED:Lombok function is used to reduce the code

--- Lms_database/src/main/java/com/lms/model/User.java
+++ Lms_database/src/main/java/com/lms/model/User.java
@@ -5,13 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import org.springframework.stereotype.Component;
-@Component
-@Entity
+
+import lombok.Data;
+
+@Entity(name = "user_details")
 @Table(name = "user_details")
+@Data
 public class User 
 {
-	//VARIABLES
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
@@ -31,82 +32,4 @@ public class User
 	private LocalDateTime creator_stamp;
 
 	private String creator_user;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFirst_name() {
-		return first_name;
-	}
-
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getLast_name() {
-		return last_name;
-	}
-
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public long getContact_number() {
-		return contact_number;
-	}
-
-	public void setContact_number(long contact_number) {
-		this.contact_number = contact_number;
-	}
-
-	public boolean getVerified() {
-		return verified;
-	}
-
-	public void setVerified(boolean verified) {
-		this.verified = verified;
-	}
-
-	public LocalDateTime getCreator_stamp() {
-		return creator_stamp;
-	}
-
-	public void setCreator_stamp(LocalDateTime creator_stamp) {
-		this.creator_stamp = creator_stamp;
-	}
-
-	public String getCreator_user() {
-		return creator_user;
-	}
-
-	public void setCreator_user(String creator_user) {
-		this.creator_user = creator_user;
-	}
-
-
-
-
-
-
 }
